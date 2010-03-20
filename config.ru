003aa1a49ff26cda638725491ee4cd7ff5155519
Steven Wilkin <stevenwilkin@gmail.com>
2010-03-20 16:24:49 +0000

    include haml & sass, use basic layout with css reset & jquery

--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'sass'
 require 'hometime'
 
 run HomeTime
--- hometime.rb
+++ hometime.rb
@@ -1,5 +1,14 @@
 class HomeTime < Sinatra::Base
+
+	set :public, File.join(File.dirname(__FILE__), 'public')
+
 	get '/' do
-		'Hello World!'
+		haml :index
 	end
+
+	get '/css/:stylesheet.css' do
+		content_type 'text/css', :charset => 'utf-8'
+		sass params[:stylesheet].to_sym
+	end
+
 end
