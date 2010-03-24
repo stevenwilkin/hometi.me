2eba484c5eefcccd010db2c3ce51870c4307b47b
Steven Wilkin <stevenwilkin@gmail.com>
2010-03-24 16:12:19 +0000

    Google Analytics

--- public/js/hometime.js
+++ public/js/hometime.js
@@ -145,4 +145,13 @@ var HomeTime = (function(){
 
 })();
 
+var GoogleAnalytics = (function(){
+	window._gaq = window._gaq || [];
+	window._gaq.push(['_setAccount', 'UA-2764682-3']);
+	window._gaq.push(['_trackPageview']);
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 $(HomeTime.init);
